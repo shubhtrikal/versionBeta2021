f11cd6bdd3c6d4d3f5e396100583f4e61b95fe70
Abhishek Nagar <abhisheknagar0110@gmail.com>
2021-09-29 23:40:17 +0530

    added: apply with devfolio on go to dashboard button

---
+++ js/dev.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let devfolioOptions = {
+      buttonSelector: "#devfolio-apply-now",
+      key: "test" // put the hackathon key here
+    };
+  
+    let script = document.createElement("script");
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+  
+    script.onload = function () {
+      new Devfolio(devfolioOptions);
+    };
+  
+    script.onerror = function () {
+      document
+        .querySelector(devfolioOptions.buttonSelector)
+        .addEventListener("click", function () {
+          window.location.href =
+            "https://devfolio.co/external-apply/" + devfolioOptions.key;
+        });
+    };
+  });
