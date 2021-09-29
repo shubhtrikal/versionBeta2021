b8782882f71602eea682f8b4b9c412c3a6100256
shubh asati <shubhtrikal9876@gmail.com>
2021-09-30 04:12:04 +0530

    feat: animation active nav link

---
+++ js/activeNav.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+    $('.nav-links li a').click(function (event) {
+        $('.nav-links li a').removeClass('active');
+        $(this).addClass('active');
+        thisAttrHref = $(this).attr('href');
+        thisAttrHrefOffset = $(thisAttrHref).offset().top;
+        $('html,body').animate({ scrollTop: thisAttrHrefOffset });
+        event.preventDefault();
+    });
+});
+
+$(window).scroll(function () {
+    $('section').each(function(){
+        containerRowTop = $(this).offset().top;
+        if($(document).scrollTop()+200 > containerRowTop){
+            thisOffset = $(this).attr('id');
+            $('section').removeClass('active');
+            ActiveID = $(this).addClass('active').attr('id');
+        }
+    });
+    $('.nav-links li').each(function(index, el){
+        thisChildren = $(this).children('a');
+        thisChildrenHref = $(this).children('a').attr('href');
+        if(thisChildrenHref === '#' +ActiveID){
+            $('.nav-links li a').removeClass('active');
+            $(thisChildren).addClass('active');
+        }
+    });
+});
