756d61fd183e68fbc4bde8af6a7c9c52fa77590e
shubh asati <shubhtrikal9876@gmail.com>
2021-09-29 17:36:32 +0530

    fix: updown scroll

--- js/app.js
+++ js/app.js
@@ -50,12 +50,10 @@ const navScroll = () => {
             nav.classList.remove('nav-color');
         }
 
-        if(window.scrollY > 700)
-        {
+        if (window.scrollY > 700) {
             upDownScroll.style.display = "block";
         }
-        else
-        {
+        else {
             upDownScroll.style.display = "none";
         }
     }
@@ -64,12 +62,10 @@ const navScroll = () => {
 const navClose = () => {
     for (var x = 0; x < linkMain.length; x++) {
         linkMain[x].addEventListener('click', () => {
-            if(navLinks.classList.contains('nav-active'))
-            {
+            if (navLinks.classList.contains('nav-active')) {
                 navLinks.classList.toggle('nav-active');
             }
-            if(body.classList.contains('fixed-body'))
-            {
+            if (body.classList.contains('fixed-body')) {
                 body.classList.toggle('fixed-body');
             }
 
@@ -77,18 +73,53 @@ const navClose = () => {
                 link.style.animation = '';
             });
 
-            if(burger.classList.contains('toggle'))
-            {
+            if (burger.classList.contains('toggle')) {
                 burger.classList.toggle('toggle');
             }
         });
     }
 }
 
-
 navScroll();
 navSlide();
 navClose();
+
+
+
+// const navActive = () => {
+//     let position = window.scrollY + 100;
+//     linkMain.forEach(navLink => {
+//         if (!navLink.hash) return
+//         let section = select(navLink.hash)
+//         if (!section) return
+//         if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+//             navLink.classList.add('activeNavClass')
+//         } else {
+//             navLink.classList.remove('activeNavClass')
+//         }
+//     })
+// }
+
+// window.addEventListener('load', navActive)
+// onscroll(document, navActive)
+
+
+// let navbarlinks = select('#navbar .scrollto', true)
+// const navbarlinksActive = () => {
+//     let position = window.scrollY + 200
+//     navbarlinks.forEach(navbarlink => {
+//         if (!navbarlink.hash) return
+//         let section = select(navbarlink.hash)
+//         if (!section) return
+//         if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+//             navbarlink.classList.add('active')
+//         } else {
+//             navbarlink.classList.remove('active')
+//         }
+//     })
+// }
+// window.addEventListener('load', navbarlinksActive)
+// onscroll(document, navbarlinksActive)
 
 
 
