716690855cfa45b424dd383fe684dc36fe75547a
22tushar <tushargehlot123@gmail.com>
2021-10-01 10:52:02 +0530

    update preloader

--- js/app.js
+++ js/app.js
@@ -86,7 +86,24 @@ navClose();
 
 
 
-window.addEventListener('load', setTimeout(() => {
-    loader.classList.add('preload-finish');
-}, 3000));
+// window.addEventListener('load', setTimeout(() => {
+//     loader.classList.add('preload-finish');
+// }, 3000));
 
+
+$(document).ready(function() {
+  
+    setTimeout(function() {
+      $('#ctn-preloader').addClass('loaded');
+      // Una vez haya terminado el preloader aparezca el scroll
+      $('body').removeClass('no-scroll-y');
+  
+      if ($('#ctn-preloader').hasClass('loaded')) {
+        // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
+        $('#preloader').delay(8000).queue(function() {
+          $(this).remove();
+        });
+      }
+    }, 9000);
+    
+  });
