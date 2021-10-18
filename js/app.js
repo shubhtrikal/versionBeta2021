c7c6a1d8ad1f41ab1f664c52d2d1fb002a31a7df
Abhishek Nagar <abhisheknagar0110@gmail.com>
2021-10-19 00:04:32 +0530

    feat:sponsor and update countdown

--- js/app.js
+++ js/app.js
@@ -117,7 +117,7 @@ $(document).ready(function() {
   function makeTimer() {
 
 	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-		var endTime = new Date("18 October 2021 19:27:00 GMT+01:00");		
+		var endTime = new Date("23 October 2021 23:59:00 GMT+01:00");		
 			endTime = (Date.parse(endTime) / 1000);
 
 			var now = new Date();
